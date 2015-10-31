58c0ed0000a5a2bd0a609ae9abbf76c527fe71f8
Adam Nygate <a.nygate@outlook.com>
2015-10-31 20:02:54 +0800

    Fix for addSignature function

--- DECA/DriveScanner.cs
+++ DECA/DriveScanner.cs
@@ -126,7 +126,7 @@ namespace DECA
                     //Add the list of signature into the signature library located within the dll
                     for (int i = 0; i <= SignatureLibrary.Signature.Length - 1; i++)
                     {
-                        addSignature(DriveScannerPointer, (UInt32)i, (UInt32)SignatureLibrary.Signature.ToArray()[i].HeaderSignature.Length, SignatureLibrary.Signature.ToArray()[i].HeaderSignature);
+                        addSignature(DriveScannerPointer, (UInt32)i, (UInt32)SignatureLibrary.Signature.ToArray()[i].HeaderSignature.Length, ConvertHexToString(SignatureLibrary.Signature.ToArray()[i].HeaderSignature));
                     }
                 }
                 catch (Exception e)
